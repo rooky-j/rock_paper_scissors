0e9e734e85d93ae51bcc15683ce5ea12957c1208
rooky-j <jamie.rookyard@live.co.uk>
2019-08-10 17:48:20 +0100

    Completed round function, just need to work out how to make it into a game...

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Scores
+    let playerScore = 0,
+        computerScore = 0;
+
+//Function to randomly decide Computer's Move
 function computerPlay() {
     let cMove = Math.random();
         if (cMove < .34) {
@@ -10,11 +15,46 @@ function computerPlay() {
     return(cMove);     
 }
 
-function playRound() {
-    let playerChoice = prompt('Rock, Paper or Scissors?').toLowerCase();
-    let computerChoice = computerPlay(); 
-
-
+//Function for Each Round
+function playRound(playerChoice, computerChoice) {
+    //Draw if both choices are equal
+        if  (playerChoice === computerChoice) {
+        winner = "It's a tie";
+        console.log('Player chose ' + playerChoice);
+        console.log('Computer chose ' + computerChoice);
+        console.log('It\'s a draw!');
+    //Rules for Player Win
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors'|| playerChoice === 'paper' && computerChoice === 'rock' || playerChoice === 'scissors' && computerChoice === 'paper') {
+        playerScore++;
+        console.log('Player chose ' + playerChoice);
+        console.log('Computer chose ' + computerChoice);
+        console.log('Player Wins!');
+    //Rules for Computer Win
+    } else if (playerChoice === 'rock' && computerChoice === 'paper'|| playerChoice === 'paper' && computerChoice === 'scissors' || playerChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore++;
+        console.log('Player chose '+ playerChoice);
+        console.log('Computer chose ' + computerChoice);
+        console.log('Computer Wins!');
+    //Message if Player hasn't entered Rock, Paper or Scissors
+    } else {
+        console.log('Please choose \'Rock, Paper or Scissors\'...');
+    }
 }
 
-playRound();
+function game(){
+    let playerChoice = prompt('Rock, Paper or Scissors?').toLowerCase();
+    let computerChoice = computerPlay(); 
+    //Start Round
+    while (playerScore <= 5 || computerScore <= 5) {
+        playRound(playerChoice, computerChoice);
+    }
+    //Win Conditions
+    if (playerScore === 5) {
+        console.log('Player Wins!');
+    }
+    if (computerScore === 5) {
+        console.log('Computer wins :(');
+    }
+}
+
+game();
